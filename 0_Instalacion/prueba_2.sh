049f3657e7957442139c6b3ce565d2709296ea40
Federico Esteban <federico.esteban@gmail.com>
2021-09-03 18:56:07 -0300

    Update prueba_2.sh

--- 0_Instalacion/prueba_2.sh
+++ 0_Instalacion/prueba_2.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
-# Comprobar que ffmpeg está presente y es capaz de crear una película MP4 o WEBM
+# Comprobar que ffmpeg está presente y es capaz de crear una MP4
 cat << EOF > main.sh
 gmt begin
 	echo \${MOVIE_FRAME} | gmt text -R0/1/0/1 -JX10c -F+f200p+cCM -B0 -X0 -Y0
 gmt end
 EOF
-gmt movie main.sh -Ncontar -T25 -C10cx10cx30 -Fmp4 -Gred -D4 -Zs -Fgif
+gmt movie main.sh -Ncontar -T25 -C10cx10cx30 -Fmp4 -Gred -D4 -Zs
+rm main.sh
