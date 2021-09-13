3ab98d9ee35d3b4697efa3896b5c49610a2244c3
Federico Esteban <federico.esteban@gmail.com>
2021-09-13 15:21:01 -0300

    Add files via upload

---
+++ 1_Ejercicios/02_FisicoPolitico_02/03_MapaPolitico_Sudamerica.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+
+#	Definir variables del mapa
+#	-----------------------------------------------------------------------------------------------------------
+#	Titulo del mapa
+	title=03_MapaPolitico_Sudamerica
+	echo $title
+
+#	Region: Sudamérica
+	REGION=-85/-33/-58/15
+
+#	Proyeccion Conica. (lon0/lat0/lat1/lat2/width) Proyeccion Albers (B); Lambert (L): Equidistant (D).
+#	PROJ=D-60/-30/-40/0/15c
+	PROJ=B-60/-30/-40/0/15c
+
+#	Parametros por Defecto
+#	-----------------------------------------------------------------------------------------------------------
+#	Sub-seccion GMT
+	gmtset GMT_VERBOSE w
+
+#	-----------------------------------------------------------------------------------------------------------
+#	Iniciar sesion y tipo de figura
+gmt begin $title png
+
+#	Setear la region y proyeccion
+	gmt basemap -R$REGION -J$PROJ -B+n
+
+#	Resaltar paises DCW (AR: Argentina soberana, FK: Malvinas, GS: Georgias del Sur y Sandwich del Sur, CO: Colombia)
+	gmt coast -EAR,FK,GS+grosybrown2+p
+
+#	Pintar areas húmedas: Oceanos (-S) y Lagos (-Cl/)f
+	color=117/197/240
+	gmt coast -Sdodgerblue2 -Cl/$color
+
+#	Dibujar Bordes Administrativos. N1: paises. N2: Provincias, Estados, etc. N3: limites marítimos (Nborder[/pen])
+	gmt coast -Df -N1/0.75
+	gmt coast -Df -N2/0.25,-.
+
+#	Dibujar Linea de Costa (level/, where level is 1-4 and represent coastline, lakeshore, island-in-lake shore, and lake-in-island-in-lake shore)
+	gmt coast -Df -W1/0.25
+
+#	Dibujar rios -Iriver[/pen] 
+#	0 = Double-lined rivers (river-lakes)
+#	1 = Permanent major rivers
+#	2 = Additional major rivers
+#	3 = Additional rivers
+#	4 = Minor rivers
+	gmt coast -Df -I0/thin,$color
+	gmt coast -Df -I1/thinner,$color
+	gmt coast -Df -I2/thinner,$color,-
+	gmt coast -Df -I3/thinnest,$color,-...
+	gmt coast -Df -I4/thinnest,$color,4_1:0p
+
+#	Dibujar frame
+	gmt basemap -Baf
+
+#	-----------------------------------------------------------------------------------------------------------
+#	Cerrar la sesion y mostrar archivo
+gmt end show
+
+	rm temp_*
+
+#	Ejercicios Sugeridos:
+#	1. Modificar REGION para que abarque a Europa.
+#	2. Modificar el color de los rios (variable $color).
+#	3. Modificar las lineas de os rios (ancho, estilo de linea).
---
+++ 1_Ejercicios/02_FisicoPolitico_02/04_MapaPolitico_Argentina.sh
@@ -0,0 +1,92 @@
+#!/bin/bash
+
+#	Definir variables del mapa
+#	-----------------------------------------------------------------------------------------------------------
+#	Titulo del mapa
+	title=04_MapaPolitico_Argentina
+	echo $title
+
+#	Region: Buenos Aires
+	REGION=-64/-42/-56/-33r
+#	Region: Argentina
+#	REGION=-78/-50/-56/-21
+	REGION=-81/-55/-53/-21r
+
+#	Proyecciones Cilindricas: 
+#	(C)assini, C(y)lindrical equal area: Lon0/lat0/Width
+#	Miller cylindrical (J): Lon0/Width
+#	(M)ercartor, E(q)udistant cilindrical, (T)ransverse Mercator: (Lon0(/Lat0/))Width
+#	(U)TM: Zone/Width
+#	PROJ=C-65/-35/13c
+#	PROJ=J-65/13c
+	PROJ=T-60/-30/13c
+#	PROJ=M15c
+#	PROJ=U-20/13c
+
+#	Parametros por Defecto
+#	-----------------------------------------------------------------------------------------------------------
+#	Sub-seccion GMT
+	gmt set GMT_VERBOSE w
+
+#	-----------------------------------------------------------------------------------------------------------
+#	Iniciar sesion y tipo de figura
+gmt begin $title png
+
+#	Setear la region y proyeccion
+	gmt basemap -R$REGION -J$PROJ -B+n
+
+#	Pintar areas húmedas: Oceanos (-S) y Lagos (-Cl/) y Rios-Lagos (-Cr/)
+	color=dodgerblue2
+	gmt coast -Df -S$color -Cl/green -Cr/red
+
+#	Resaltar paises DCW (AR: Argentina soberana, FK: Malvinas, GS: Georgias del Sur y Sandwich del Sur)
+	gmt coast -EAR,FK,GS+grosybrown2+p
+
+#	Dibujar Bordes Administrativos. N1: paises. N2: Provincias, Estados, etc. N3: limites marítimos (Nborder[/pen])
+	gmt coast -Df -N1/0.75
+#	gmt coast -Df -N2/0.25,-.
+
+#	Dibujar Linea de Costa (level/, where level is 1-4 and represent coastline, lakeshore, island-in-lake shore, and lake-in-island-in-lake shore)
+	gmt coast -Df -W1/0.25
+
+#	Datos Instituto Geografico Nacional Argentino (IGN)
+#	-----------------------------------------------------------------------------------------------------------
+#	Descripcion plot: Lineas (-Wpen), Puntos (-Ssímbolo/size), Relleno simbolos o polígonos (-Gfill).
+#	-G: pinta el area definida por los puntos.
+#	-G y -W: Pinta el área y dibuja las lineas del borde
+#	-W: Dibuja las lineas del borde 
+#	-S: dibuja los simbolos sin relleno
+#	-S -G: dibuja simbolos con relleno
+#	-S -W -G: dibuja simbolos con relleno y borde
+
+#	Cursos y Cuerpos de Agua
+	gmt plot "IGN/areas_de_aguas_continentales_perenne.shp" -G$color
+#	gmt plot "IGN/lineas_de_aguas_continentales_perenne.shp" -Wfaint,blue
+
+#	Limites Interprovincial
+	gmt plot "IGN/linea_de_limite_070111.shp" -Wthinner,black,-.
+
+# 	Red vial y ferroviaria
+	gmt plot "IGN/RedVial_Autopista.gmt"        			-Wthinnest,black
+	gmt plot "IGN/RedVial_Ruta_Nacional.gmt"    			-Wthinnest,black
+	gmt plot "IGN/RedVial_Ruta_Provincial.gmt"  			-Wfaint,black
+	gmt plot "IGN/lineas_de_transporte_ferroviario_AN010.shp"     -Wthinnest,darkred
+
+#	Pueblos y Ejidos Urbanos. -SsimboloTamaño. Simbolos: A (star), C (Círculo), D (Diamante), G (octagono), H (hexagono), I (triangulo invertido), N (pentagono), S (cuadrado), T (triangulo).
+#	Tamaño: diámetro del círculo (Mayuscula: misma área que el circulo; Minúscula (diámetro del círculo que abarca a las símbolos)
+	gmt plot "IGN/puntos_de_asentamientos_y_edificios_localidad.shp" -Sc0.04 -Ggray19
+	gmt plot "IGN/areas_de_asentamientos_y_edificios_020105.shp"     -Wfaint -Ggreen
+
+#	Dibujar frame
+	gmt basemap -Baf
+
+#	-----------------------------------------------------------------------------------------------------------
+#	Cerrar la sesion y mostrar archivo
+gmt end show
+
+	rm temp_* gmt.*
+
+#	Ejercicios Sugeridos:
+#	1. Modificar REGION para que abarque a Europa.
+#	2. Modificar el color de los rios (variable $color).
+#	3. Modificar las lineas de los rios (ancho, estilo de linea).
