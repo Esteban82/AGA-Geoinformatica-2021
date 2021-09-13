9eb2e8bea96141a170c8da0bd0cffe9c4f81ed7a
Federico Esteban <federico.esteban@gmail.com>
2021-09-13 14:24:39 -0300

    Add files via upload

---
+++ 1_Ejercicios/01_FisicoPolitico_01/EJ1.2_Hemisferico.sh
@@ -0,0 +1,68 @@
+#!/usr/bin/env bash
+
+#	Definir variables del mapa
+#	-----------------------------------------------------------------------------------------------------------
+#	Titulo del mapa
+	title=EJ1.2_Hemisferico
+	echo $title
+
+#	Proyecciones acimutales requieren 3 parametros + 1 opcional (lon0/lat0[/horizon]/width
+#	L(a)mbert Equal Area; (S)tereographic; Orto(g)rafica; (E)quidistante
+	PROJ=S-65/-30/90/15c
+	PROJ=G-65/-30/90/15c
+
+#	Region geografica del mapa (W/E/S/N) d=-180/180/-90/90 g=0/360/-90/90
+	REGION=d
+
+#	Parametros por Defecto
+#	-----------------------------------------------------------------------------------------------------------
+#	Sub-seccion GMT
+	gmt set GMT_VERBOSE w
+
+#	-----------------------------------------------------------------------------------------------------------
+#	Iniciar sesion y tipo de figura
+gmt begin $title png
+
+#	Setear la region y proyeccion
+	gmt basemap -R$REGION -J$PROJ -B+n
+
+#	Pintar areas secas (-G). Resolucion datos full (-Df)
+	gmt coast -Df -G200
+
+#	Dibujar Antartida_Argentina. Los meridianos 74° Oeste y 25° Oeste y el paralelo 60° Sur y el Polo Sur.
+#	Dibujar archivo con borde (-Wpen), Relleno (-Gfill), Lineas siguen meridianos (-Am), Cerrar polígonos (-L)
+	gmt plot -L -Am -Grosybrown2 -W0.25  <<- EOF
+	-74 -60
+	-74 -90
+	-25 -60
+	EOF
+
+#	Pintar areas húmedas: Oceanos (-S) y Lagos y Rios (-C)
+#	gmt coast -Sdodgerblue2
+	gmt coast -Sdodgerblue2 -C200
+#	gmt coast -Swhite -C200
+
+#	Dibujar Paises (1 paises, 2 estados/provincias en America, 3 limite maritimo)
+	gmt coast -N1/0.2,-
+
+#	Dibujar Linea de Costa
+	gmt coast -Df -W1/
+
+#	Resaltar paises DCW (-E). Codigos ISO 3166-1 alph-2. (AR: Argentina soberana, FK: Malvinas, GS: Georgias del Sur). Pintar de color (+g), Dibujar borde (+p). 
+	gmt coast -EAR,FK,GS+grosybrown2+p
+	
+#	Resaltar provincias DCW (-E). Codigos ISO 3166-2:AR (X: Cordoba, L: La Pampa, J: San Juan).
+#	gmt coast -EAR.X,AR.L,AR.J+gorange+p
+
+#	Dibujar marco del mapa 
+	gmt basemap -Bg
+
+#	Dibujar paralelos principales y meridiano de Greenwich a partir del archivo paralelos.txt -Am (lineas siguen meridianos)
+	gmt plot 'Paralelos.txt' -Ap -W0.50,.-
+
+#	-----------------------------------------------------------------------------------------------------------
+#	Cerrar la sesion y mostrar archivo
+gmt end show
+
+#	Borrar archivos tempostales
+	rm gmt.*
