6f2d71c700219875c0b4b4008d145aa819856219
FedericoEsteban <federico.esteban@gmail.com>
2021-10-13 23:23:59 -0300

    08 Ejercicios sugeridos

--- 1_Ejercicios/03C_Tectonico/08_Tectonico.sh
+++ 1_Ejercicios/03C_Tectonico/08_Tectonico.sh
@@ -7,7 +7,7 @@
 #	Definir variables del mapa
 #	-----------------------------------------------------------------------------------------------------------
 #	Titulo del mapa
-	title=07_Tectonico
+	title=08_Tectonico
 	echo $title
 
 #	Region Geografica
@@ -42,15 +42,15 @@ gmt begin $title png
 #	Datos Tectonicos
 #	-----------------------------------------------------------------------------------------------------------
 #	Plates Project: Limite de Placas
-	gmt plot "Datos\transform.gmt" -A -W0.4,black -Sf2.0c/0.25+l+s -Gblack
-	gmt plot "Datos\trench.gmt"    -A -W0.4,green -Sf0.6c/0.15+l+t -Ggreen
-	gmt plot "Datos\ridge.gmt"     -A -W1.0,red
-	gmt plot "Datos\ridge.gmt"     -A -W0.4,white
+	gmt plot "Datos/transform.gmt" -A -W0.4,black -Sf2.0c/0.25+l+s -Gblack
+	gmt plot "Datos/trench.gmt"    -A -W0.4,green -Sf0.6c/0.15+l+t -Ggreen
+	gmt plot "Datos/ridge.gmt"     -A -W1.0,red
+	gmt plot "Datos/ridge.gmt"     -A -W0.4,white
 
 #	Plates Project: LIPs. Dibujar patrones
-#	gmt plot "Datos\LIPS.2011.gmt" -A -Gp300/29 
-#	gmt plot "Datos\LIPS.2011.gmt" -A -Gp300/29
-	gmt plot "Datos\LIPS.2011.gmt" -A -Gp300/29:BivoryFred3 
+#	gmt plot "Datos/LIPS.2011.gmt" -A -Gp300/29 
+#	gmt plot "Datos/LIPS.2011.gmt" -A -Gp300/29
+	gmt plot "Datos/LIPS.2011.gmt" -A -Gp300/29:BivoryFred3 
 
 #	Dibujar Sismos
 #	-----------------------------------------------------------------------------------------------------------
@@ -61,24 +61,24 @@ gmt begin $title png
 
 #	Dibujar Sismos del USGS segun magnitud (-Scp), y color segun profundidad (-C). 
 #	-Scp: El tamaño del circulo corresponde a la magnitud medida en puntos tipograficos. 
-#	gmt plot "Sismos\USGS.txt"  -W0.1 -Gred -Sc0.1c		# Tamaño y color fijo 
-#	gmt plot "Sismos\USGS.txt"  -W0.1 -C    -Sc0.1c    	# Color variable (3 columna) y tamaño fijo
-	gmt plot "Sismos\USGS.txt"  -W0.1 -C 	-Scp		# Color (3a col) y tamaño (4a col) variable 
+#	gmt plot "Sismos/USGS.txt"  -W0.1 -Gred -Sc0.1c		# Tamaño y color fijo 
+#	gmt plot "Sismos/USGS.txt"  -W0.1 -C    -Sc0.1c    	# Color variable (3 columna) y tamaño fijo
+	gmt plot "Sismos/USGS.txt"  -W0.1 -C 	-Scp		# Color (3a col) y tamaño (4a col) variable 
 
 #	Convertir tabla de datos 	
 #	gmt convert query.csv -h1 -i2,1,3,4 > Sismos.txt
 
 #	Plotear datos originales (ver -h y -i)
-#	gmt plot "Sismos\query.csv" -W.1 -C -Scp -h1 -i2,1,3,4     
+#	gmt plot "Sismos/query.csv" -W.1 -C -Scp -h1 -i2,1,3,4     
 
 #	Dibujar Mecanismos Focales
 #	-----------------------------------------------------------------------------------------------------------
 #	Datos Global CMT. Tamaño Proporcional a la magnitud (-M: Tamaño Homogeneo)
-#	gmt meca "Mecanismos_Focales\CMT_1976-2013.txt"   -Sd0.15/6
-#	gmt meca "Mecanismos_Focales\CMT_1976-2013.txt"   -Sd0.15/0 -Gred          
-#	gmt meca "Mecanismos_Focales\CMT_2014-2015.txt"   -Sd0.15/0 -Gorange
-#	gmt meca "Mecanismos_Focales\CMT_*.txt"           -Sd0.15/0 -Gorange
-	gmt meca "Mecanismos_Focales\CMT_*.txt"           -Sd0.15/0 -Gorange -M
+#	gmt meca "Mecanismos_Focales/CMT_1976-2013.txt"   -Sd0.15/6
+#	gmt meca "Mecanismos_Focales/CMT_1976-2013.txt"   -Sd0.15/0 -Gred          
+#	gmt meca "Mecanismos_Focales/CMT_2014-2015.txt"   -Sd0.15/0 -Gorange
+#	gmt meca Mecanismos_Focales/CMT_*.txt"           -Sd0.15/0 -Gorange
+	gmt meca Mecanismos_Focales/CMT_*.txt           -Sd0.15/0 -Gorange -M
 	
 #	-----------------------------------------------------------------------------------------------------------
 #	Dibujar Escala en el mapa centrado en -Lg Lon0/Lat0, calculado en meridiano (+c), ancho (+w), elegante(+f), unidad arriba de escala (+l), unidad con los valores (+u)
@@ -93,6 +93,7 @@ gmt begin $title png
 
 	rm gmt.conf
 #	-----------------------------------------------------------------------------------------------------------
+#
 #	Ejercicios Sugeridos:
 #	1. En las lineas 44-45 se utilza plot para dibujar lines correspodientes a fallas de rumbo e inversas. Modificar el formato (ver manpage de plot el formato de -Sf).
 #	2. En las lineas 72-74 se crea un cpt que luego se utiliza para pintar los sismos. Modificar los rangos de valores de cpt y los colores asginados. 
