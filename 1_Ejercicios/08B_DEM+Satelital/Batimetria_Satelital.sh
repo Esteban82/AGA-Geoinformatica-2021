d7fb272410857267bfaf908cf003fa11f2dc5bd2
FedericoEsteban <federico.esteban@gmail.com>
2021-11-12 11:48:33 -0300

    ckas

---
+++ 1_Ejercicios/08B_DEM+Satelital/Batimetria_Satelital.sh
@@ -0,0 +1,69 @@
+#!/usr/bin/env bash
+clear
+
+#	Temas a ver
+#   1. Combinar imagenes satelitales y topografia/batimetria.
+
+#	Definir variables del mapa
+#	-----------------------------------------------------------------------------------------------------------
+#	Titulo del mapa
+	title=Batimetria_Satelital
+	echo $title
+
+#	Region: Argentina
+#	REGION=-72/-64/-35/-30
+#	REGION=AR,BR,CO
+#	REGION=AR,CL,GS
+    REGION==SA
+
+#	Proyeccion Mercator (M)
+	PROJ=M15c
+
+#	Resoluciones grillas: 01d, 30m, 20m, 15m, 10m, 06m, 05m, 04m, 03m, 02m, 01m, 30s, 15s, 03s, 01s.
+	RES=03m
+
+#	Fuente a utilizar
+	GRD=@earth_relief_$RES
+    SAT=@earth_day_$RES
+
+#	Dibujar mapa
+#	-----------------------------------------------------------------------------------------------------------
+#	Iniciar sesion y tipo de figura
+gmt begin $title png
+	
+#	Setear la region y proyeccion
+	gmt basemap -R$REGION -J$PROJ -B+n
+
+#	Idem y agrega efecto de sombreado. a= azimut. nt1=metodo de ilumninacion
+	gmt grdimage $GRD -I
+    
+#	Agregar escala de colores a partir de CPT (-C). Posición (x,y) +wlargo/ancho. Anotaciones (-Ba). Leyenda (+l). 
+	gmt colorbar -DJRM -I -Baf -By+l"km" -W0.001 -F+gwhite+p+i+s -GNaN/0
+
+#   Recorte (visual)
+#   *************************************************************
+	gmt coast -G    # Recorte para continentes
+#    gmt coast -S    # Recorte para oceanos
+
+#   Graficar imagen satelital
+	gmt grdimage $SAT
+
+#   Finalizar recorte
+	gmt coast -Q 
+#   *************************************************************
+
+#   -------------------------------------------------------------------------------------------------
+#	Dibujar frame
+	gmt basemap -Bxaf -Byaf
+
+#	Dibujar Linea de Costa (W1)
+	gmt coast -N1/faint
+
+#	-----------------------------------------------------------------------------------------------------------
+#	Cerrar el archivo
+gmt end
+
+#	Ejercicios Sugeridos
+#	1. Modificar region del mapa y la resolucion del mapa
+#	2. Modificar azimuth de la fuente de iluminuacion (az en linea 43)
+#	3. Probar las distintas opciones para la escala de color (lineas 45 a 55).
